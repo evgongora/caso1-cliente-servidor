f806b5063a64eea1321eacbe56d6d7f75bb2a1b7
evgongora <evasquezgongora@gmail.com>
2025-02-24 20:14:13 -0600

    first commit

---
+++ src/main/java/com/mycompany/caso1cliente/servidor/Caso1clienteServidor.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.caso1cliente.servidor;
+
+/**
+ *
+ * @author erickvasquezgongora
+ */
+public class Caso1clienteServidor {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
